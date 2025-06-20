6b991867736f88cb8a81a7f35fbd143c894a55a9
Leo-Avelar <leonardo.savelar@gmail.com>
2025-06-20 09:15:49 -0300

    add new routes

--- src/routes/customer.routes.js
+++ src/routes/customer.routes.js
@@ -5,7 +5,10 @@ const customerController = require('../controllers/customer.controller');
 router.post('/', customerController.createCustomer);
 router.get('/', customerController.getAllCustomers);
 router.get('/:id', customerController.getCustomerById);
+router.get('/email/:email', customerController.getCustomerIdByEmail);
 // router.put('/:id', customerController.updateCustomer);
 // router.delete('/:id', customerController.deleteCustomer);
+router.put('/vehicle', customerController.addVehicleToCustomer);
+router.delete('/vehicle', customerController.removeVehicleFromCustomer);
 
 module.exports = router;
--- src/routes/service.routes.js
+++ src/routes/service.routes.js
@@ -6,5 +6,8 @@ router.post('/entry', serviceController.recordEntry);
 router.put('/exit/:license_plate', serviceController.recordExit);
 router.get('/', serviceController.getAllServiceRecords);
 router.get('/calculate-fee/:license_plate', serviceController.calculateCurrentFee);
+router.get('/entry-time/:license_plate', serviceController.getEntryTimeByLicensePlate);
+router.get('/plate/:license_plate', serviceController.getServiceRecordByLicensePlate);
+router.get('/:customer_id', serviceController.getServiceRecordsByCustomerId);
 
 module.exports = router;
